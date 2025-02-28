372be7784f0f196a3a4836a167318807b079a028
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-28 14:35:10 +0800

    App.js: Mounted Video components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
 
+import { Video } from './components'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="videoContainer">
+        <Video />
+        <Video />
+      </div>
     </div>
   )
 }
